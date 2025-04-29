c23f0a7ae1ba2f16f34d805bd2ca31ecf15350e9
Guilherme020205 <seemannguilherme@gmail.com>
2025-04-29 13:15:50 -0300

    tailwind new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 function App() {
-
   return (
-    <>
-      <p className='bg-red-300'>Ola mundo</p>
-    </>
-  )
+    <div className="w-52">
+      <div className="flex flex-row">
+        <p className="bg-red-300 w-full flex justify-center">1</p>
+        <p className="bg-green-300 w-full flex justify-center">2</p>
+      </div>
+      <p className="bg-gray-400 flex justify-center">3</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import './output.css'
+import './index.css' 
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h1 className='font-bold'>teste</h1>
-    <App/>
+      <App/>
   </StrictMode>,
 )
