c21f6b9768c2a26c9dbd803c4bcd73234a606482
Guilherme020205 <seemannguilherme@gmail.com>
2025-05-01 21:28:00 -0300

    now header

--- frontend/src/components/header/index.jsx
+++ frontend/src/components/header/index.jsx
@@ -1,22 +1,88 @@
-const logo =
-  "";
+import React, { useRef, useContext } from "react";
+import { Button } from "primereact/button";
+import { Menu } from "primereact/menu";
+import { Toast } from "primereact/toast";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import "primeicons/primeicons.css";
+import "./cssmenu.css";
+import { AuthContext } from "../../context/AuthContext";
+
+const logo = "";
 
 const Header = () => {
+  const menuRight = useRef(null);
+  const toast = useRef(null);
+
+  const { logout } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const items = [
+    {
+      items: [
+        {
+          label: "Sair",
+          icon: "pi pi-sign-out",
+          command: () => {
+            logout();
+            navigate("/login");
+          },
+        },
+      ],
+    },
+  ];
+
+  const location = useLocation();
 
   return (
-    <div className="bg-red-200 h-16 flex items-center px-5 justify-between">
-      <div className="flex flex-row gap-56">
-        {logo ? (
-          <img src={logo} alt="logo" className="w-40 h-10" />
-        ) : (
-          <p>Logo</p>
-        )}
-        <div className="flex flex-row gap-16">
-          <button className="bg-white w-16 max-w-16 rounded-full">1</button>
-          <button className="bg-white w-16 max-w-16 rounded-full">2</button>
-          <button className="bg-white w-16 max-w-16 rounded-full">3</button>
+    <div className="bg-blue-300 h-11 flex flex-row justify-between items-center px-[5%]">
+      {logo ? <img src={logo} alt="logo" className="w-32" /> : <p>Logo</p>}
+      <div className="flex flex-row gap-4">
+        <Link
+          to="/home"
+          className={`p-[6px] rounded-full hover:bg-blue-100 ${
+            location.pathname === "/home" ? "bg-white" : ""
+          }`}
+        >
+          <i className="pi pi-home text-xl"></i>
+        </Link>
+
+        <Link
+          to="/"
+          className={`p-[6px] rounded-full hover:bg-blue-100 ${
+            location.pathname === "/" ? "bg-white" : ""
+          }`}
+        >
+          <i className="pi pi-users text-xl"></i>
+        </Link>
+
+        <Link
+          to="/"
+          className={`p-[6px] rounded-full hover:bg-blue-100 ${
+            location.pathname === "/" ? "bg-white" : ""
+          }`}
+        >
+          <i className="pi pi-book text-xl"></i>
+        </Link>
+      </div>
+      <div>
+        <div className="card flex justify-content-center">
+          <Toast ref={toast}></Toast>
+          <Menu
+            model={items}
+            popup
+            ref={menuRight}
+            id="popup_menu_right"
+            popupAlignment="right"
+            className="bg-white rounded-lg px-3 py-1 gap-2"
+          />
+          <Button
+            icon="pi pi-align-right"
+            onClick={(event) => menuRight.current.toggle(event)}
+            aria-controls="popup_menu_right"
+            aria-haspopup
+            className="mr-2"
+          />
         </div>
-       
       </div>
     </div>
   );
--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,20 +1,7 @@
-import { useContext } from "react";
-import { AuthContext } from "./../../context/AuthContext";
-import { useNavigate } from "react-router-dom";
-
- function ScreenHome() {
-  const { logout } = useContext(AuthContext);
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    logout();
-    navigate("/login");
-  };
-
+function ScreenHome() {
   return (
-    <div>
+    <div className="py-10 px-32">
       <h2>ScreenHome</h2>
-      <button onClick={handleLogout}>Sair</button>
     </div>
   );
 }
